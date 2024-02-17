d0900fa79e7dd28bae4c8fda817bce02508eb43f
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-17 12:00:43 +0430

    init

--- app.js
+++ app.js
@@ -13,3 +13,4 @@ I just told you! You've killed me! Fry! Quit doing the right thing, you jerk! Mi
 ];
 
 const form = document.querySelector('.lorem-form')
+const amount = document.getElementById('amount')
