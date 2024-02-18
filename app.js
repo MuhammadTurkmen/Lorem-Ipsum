a496a2c1de6ffd7729f11952fd91759c9cd3c074
MuhammadTurkmen <muhammadturkmen204@gmail.com>
2024-02-18 14:16:35 +0430

    init

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ form.addEventListener('submit', function(e) {
   } 
   else {
     let tempText = text.slice(0, value) 
-    tempText = tempText.map(function(item) {
+    tempText = tempText.map(function(item) { 
       return `<p class="result">${item}</p>`
     })
   }
